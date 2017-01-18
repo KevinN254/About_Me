15a47822b591bdd959f1bceb9cc927d2f3e92652
kngunjiri <kevin.ngunjiri@autotrader.com>
2017-01-18 01:24:10 -0500

    Changed CSS format- WIP

---
+++ aboutme.js
@@ -0,0 +1,29 @@
+function show(content) {
+document.getElementById(content).style.display = 'block';
+}
+
+function hide(content) {
+    document.getElementById(content).style.display = 'none';
+}
+	
+	<h2>Experience</h2>
+
+	<p>My backround is in programming and Quality Assurance.</p>
+
+	<ul>
+		<li>JavaScript</li>
+		<li>Jquerry</li>
+		<li>HTML/CSS</li>
+	</ul>
+
+	<p>I am currently learning JAVA and working on getting better in JavaScript</p>
+
+	<p>I am taking a nanondegree course in FrontEnd Web Development with Udacity which focuses on HTML,CSS,Python and JavaScript.</p>
+
+	<h3>Work Example</h3>
+
+	<p>You can see some of my work on <a href="https://github.com/Ngunjirik91"><b>MY GITHUB</b></a></p>
+
+
+
+
